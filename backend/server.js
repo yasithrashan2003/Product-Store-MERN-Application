550bff32e49bd4109f6f6c3ae3dd311c31a11d35
Yasith Rashan <154666486+yasithrashan2003@users.noreply.github.com>
2025-02-25 13:54:40 +0530

    Connect MongoDB to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from '../config/db.js';
+
+dotenv.config();
+
 
 const app=express();
+
+app.get("/products", (req,res) => {
+    res.send("Server is ready")
+})
 
 
 
 app.listen(5000, () =>{
+    connectDB();
     console.log('server started at http://localhost:5000')
 
 });
